c443db874b277207078b442f7d57d3e3742d702b
Trygve Leithe Svalheim <trygvels@astro.uio.no>
2017-03-01 11:46:10 +0100

    Almost done

--- oblig1a.R
+++ oblig1a.R
@@ -3,14 +3,14 @@
 library(car)
 # Read data
 no2data <- read.table("http://www.uio.no/studier/emner/matnat/math/STK4900/v17/no2.txt",sep="\t",header=TRUE)
-
+plot(no2data, main="no2data")
 # Name variables
 names(no2data)=c('no2','log.cars','temp','wind.speed','hour.of.day')
 attach(no2data)
 
 # ---------------- Oppgave 1 a ----------------
 summary(no2data)
-plot(log.cars,no2)
+plot(log.cars,no2, main="Air pollution with number of cars")
 
 # ---------------- Oppgave 1 b ----------------
 fit.both=lm(no2~log.cars, data=no2data)
@@ -19,6 +19,19 @@ abline(fit.both,col=2)
 
 # ---------------- Oppgave 1 c ----------------
 
+# Check linearity
+
+fit.both.fit=fit.both$fit
+fit.both.res= fit.both$res
+crPlots(fit.both, terms=~.,main="Residuals")
+
+# Check normality
+hist(fit.both.res)
+qqnorm(fit.both.res); qqline(fit.both.res)
+
+# Check homoscedasticy
+plot(fit.both.fit, fit.both.res, xlab="Fitted values", ylab="Residuals", main="Checking Homoscedasticy")
+abline(0,0,col=2)
 # ---------------- Oppgave 1 d ----------------
 # Log has no effect
 fit.all=lm(no2~log.cars+temp+wind.speed+hour.of.day, data=no2data)
@@ -29,7 +42,7 @@ crPlots(fit.all, terms=~.)
 # Better fit using second order terms
 model = log.cars+I(log.cars^2)+temp+wind.speed+I(wind.speed^2)+hour.of.day
 fit.all2=lm(no2~(log.cars+I(log.cars^2))+temp+(wind.speed+I(wind.speed^2))+hour.of.day)
-
+summary(fit.all2)
 # ---------------- Oppgave 1 e ----------------
 
 # Check linearity
--- oblig1b.R
+++ oblig1b.R
@@ -1,21 +1,24 @@
+library(contrast)  
 # Read data
 blood <- read.table("http://www.uio.no/studier/emner/matnat/math/STK4900/v17/blood.txt",sep=",",header=TRUE)
 
 # ---------------- Oppgave 2 a ----------------
 # Divide into 3 levels
-blood$alder = factor(blood$alder)
-blood$alder		# produces levels 1, 2, 3
+#blood$alder = factor(blood$alder)
+#blood$alder		# produces levels 1, 2, 3
 
 # Create box plot
-boxplot(blodtr~alder, data=blood,main="Blood pressure in age groups", names=c("30-45 years","46-59 years","60-75 years"))
-
+boxplot(blood$blodtr~blood$alder, data=blood,main="Blood pressure in age groups", names=c("30-45 years","46-59 years","60-75 years"))
+summary(blood$alder)
 # ---------------- Oppgave 2 b ----------------
 # One way ANOVA
-aov.blood = aov(blodtr~alder, data=blood)
+aov.blood = aov(blood$blodtr~blood$alder, data=blood)
 aov.blood
 summary(aov.blood)
 
 # ---------------- Oppgave 2 c ----------------
-# Linear Regression model
-linregrmod = lm(blodtr~alder)
-options(contrasts=c("contr.treatment","contr.poly"))
+# Regression model
+linregrmod = lm(blood$blodtr~blood$alder)
+summary(linregrmod)
+plot(linregrmod)
+
